14359cce0f864725e2e7e41b71df1b375e832287
Andrey Timonin <an-timwork@yandex.ru>
2025-12-06 08:12:59 +0300

    Modified: __init.py__

--- fresh_rotten_classification/__init__.py
+++ fresh_rotten_classification/__init__.py
@@ -3,13 +3,13 @@ from .model import SimpleCNN, ResNetClassifier, create_model
 from .lightning_module import FreshRottenClassifier
 from .train import main as train_main
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 __all__ = [
-    'FreshRottenDataset',
-    'FreshRottenDataModule',
-    'SimpleCNN',
-    'ResNetClassifier',
-    'create_model',
-    'FreshRottenClassifier',
-    'train_main'
+    "FreshRottenDataset",
+    "FreshRottenDataModule",
+    "SimpleCNN",
+    "ResNetClassifier",
+    "create_model",
+    "FreshRottenClassifier",
+    "train_main",
 ]
