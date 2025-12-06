13291557b1773b903ee6da7ad2229708dc7579ce
Andrey Timonin <an-timwork@yandex.ru>
2025-12-06 13:50:39 +0300

    Some fix

--- run_training.py
+++ run_training.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-"""Run training with Hydra configuration.
-Usage examples:
+"""Запуск обучения с Hydra конфигом.
+Примеры вызова:
     python run_training.py
-    python run_training.py model=simple_cnn
-    python run_training.py data.batch_size=64 training.learning_rate=0.0001
-    python run_training.py data.use_augmentation=false training.gpu=true
-    python run_training.py -m data.batch_size=32,64,128
+    python run_training.py training.max_epochs=1
 """
 
 # Этот файл теперь просто импортирует и запускает train.py
